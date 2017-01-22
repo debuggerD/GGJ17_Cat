e1c1baa71971709d64b3ff7599e3e0331b1a66ff
Jaeseung Ha <ipknhama@gmail.com>
2017-01-22 13:54:16 +0900

    title scene

---
+++ Cat_Unity/Assets/TitleScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleScript : MonoBehaviour {
+	public Button fireButton;
+
+	// Use this for initialization
+	void Start () {
+		fireButton.onClick.AddListener(StartGame);
+	}
+
+	IEnumerator DoStart() {
+		yield return new WaitForSeconds(2.4f);
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("OpeningScene");
+	}
+
+	void StartGame() {
+		GetComponent<AudioSource> ().Play ();
+
+		StartCoroutine (DoStart());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
